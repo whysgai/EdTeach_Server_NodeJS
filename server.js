fdce8eb28c95ad76dbb5ac475914faef62fb4659
Will Cohen <will.r.cohen@gmail.com>
2020-11-15 09:53:37 -0500

    A8 infra: Install body-parser library

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 const quizService = require("./services/quizzes.services.server")
 quizService(app);
-
 require("./services/questions.services.server")(app);
 
 app.listen(4201);
