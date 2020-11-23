d143f528e52abd06ba36696a961e941ab47874ec
Will Cohen <will.r.cohen@gmail.com>
2020-11-23 12:10:16 -0500

    A9 infra: Install mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/edteach', {useNewUrlParser: true});
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
