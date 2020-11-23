86a973df48094ebbb7e885f8f8b366352969dc55
Will Cohen <will.r.cohen@gmail.com>
2020-11-23 12:56:58 -0500

    A9 feat: Refactor to use mongo model

--- services/quizzes.service.js
+++ services/quizzes.service.js
@@ -1,13 +1,15 @@
 let quizzes = require('../models/quizzes.json');
+const quizModel = require("../models/quizzes/quiz.model");
 
-const findAllQuizzes = () => {
-    return quizzes
-}
+// const findAllQuizzes = () => {
+//     return quizzes
+// }
 
-const findQuizById = (qid) => {
-    const quiz = quizzes.find(quiz => quiz._id === qid);
-    return quiz;
-}
+const findAllQuizzes = () => quizModel.findAllQuizzes();
+
+const findQuizById = (qid) => quizModel.findQuizById(qid);
+    // const quiz = quizzes.find(quiz => quiz._id === qid);
+    // return quiz;
 
 const createQuiz = () => {
     const quiz = {_id: (new Date()).getMilliseconds().toString(), title: "New Quiz"};
