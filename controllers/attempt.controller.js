3e04802260df626be426f89264ef5d1617287572
Will Cohen <will.r.cohen@gmail.com>
2020-11-28 10:34:47 -0500

    A9 feat: Implement attempt controller

---
+++ controllers/attempt.controller.js
@@ -0,0 +1,17 @@
+const attemptService = require("../services/attempt.service");
+
+module.exports = (app) => {
+
+    const findAttemptsForQuiz = (req, res) => {
+        attemptService.findAttemptsForQuiz(req.params.qid)
+            .then(attempts => res.send(attempts));
+    }
+
+    const createAttempt = (req, res) => {
+        attemptService.createAttempt(req.params.qid, req.body)
+            .then(attempt => res.send(attempt));
+    }
+
+    app.get('/api/quizzes/:qid/attempts', findAttemptsForQuiz);
+    app.post('/api/quizzes/:qid/attempts', createAttempt);
+}
