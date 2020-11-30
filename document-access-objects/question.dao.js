f8f32ab06132eaa62517eadd190bad7d52c5878d
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 13:07:02 -0500

    A9 feat: Implement find questions for quiz via quiz knowledge

--- document-access-objects/question.dao.js
+++ document-access-objects/question.dao.js
@@ -3,13 +3,12 @@ const questionModel = require('../models/questions/question.model');
 
 const findAllQuestions = () => questionModel.find();
 
-// const findQuestionsForQuiz = (qid) => questionModel.find({"quizId": qid});
-
-// const findQuestionsForQuiz = (qid) => quizModel.findById(qid)
-//     .populate('questions').then(quiz => quiz.questions)
+const findQuestionsForQuiz = (qid) => quizModel.findById(qid)
+    .populate('questions').then(quiz => quiz.questions)
 
 
 module.exports = {
-    findAllQuestions,
-    findQuestionsForQuiz
+    findQuestionsForQuiz,
+    findAllQuestions
+
 }
