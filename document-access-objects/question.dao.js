aed7ecbae7483aacad64c8f1869c1538f32b1e91
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 16:21:02 -0500

    A9 feat: Implement create question

--- document-access-objects/question.dao.js
+++ document-access-objects/question.dao.js
@@ -3,14 +3,16 @@ const questionModel = require('../models/questions/question.model');
 
 const findAllQuestions = () => questionModel.find();
 
-
 const findQuestionsForQuiz = (qid) => quizModel.findById(qid)
     .populate('questions').then(quiz => quiz.questions)
 
 // No longer in use due to questions no longer knowing parents
 // const findQuestionsForQuiz = (qid) => questionModel.find({"quizId": qid});
 
+const createQuestion = (question) => questionModel.create(question);
+
 module.exports = {
+    createQuestion,
     findQuestionsForQuiz,
     findAllQuestions
 }
