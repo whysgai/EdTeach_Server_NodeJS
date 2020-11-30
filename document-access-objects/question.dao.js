e3ff7547f0823f52c61833f39b29bc9cf7e036e9
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 13:03:48 -0500

    A9 refactor: Depricate find questions by quiz--use find single quiz

--- document-access-objects/question.dao.js
+++ document-access-objects/question.dao.js
@@ -3,7 +3,11 @@ const questionModel = require('../models/questions/question.model');
 
 const findAllQuestions = () => questionModel.find();
 
-const findQuestionsForQuiz = (qid) => questionModel.find({"quizId": qid});
+// const findQuestionsForQuiz = (qid) => questionModel.find({"quizId": qid});
+
+// const findQuestionsForQuiz = (qid) => quizModel.findById(qid)
+//     .populate('questions').then(quiz => quiz.questions)
+
 
 module.exports = {
     findAllQuestions,
