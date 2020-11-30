0ba470b4b2c7188e048e03553b9f25739abbf222
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 13:23:05 -0500

    A9 docs: Add explanatory comments

--- document-access-objects/question.dao.js
+++ document-access-objects/question.dao.js
@@ -3,12 +3,14 @@ const questionModel = require('../models/questions/question.model');
 
 const findAllQuestions = () => questionModel.find();
 
+
 const findQuestionsForQuiz = (qid) => quizModel.findById(qid)
     .populate('questions').then(quiz => quiz.questions)
 
+// No longer in use due to questions no longer knowing parents
+// const findQuestionsForQuiz = (qid) => questionModel.find({"quizId": qid});
 
 module.exports = {
     findQuestionsForQuiz,
     findAllQuestions
-
 }
