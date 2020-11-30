929a16c067f0bae000740b8631eb5099e3ec90a3
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 13:01:02 -0500

    A9 refactor: Make quiz know its questions

--- models/quizzes/quiz.schema.js
+++ models/quizzes/quiz.schema.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const questionSchema = require('../questions/question.schema');
 
 const quizSchema = mongoose.Schema({
-    title: String
+    title: String,
+    questions: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "QuestionModel"
+    }]
 }, {collection: "quizzes"});
 
 module.exports = quizSchema
