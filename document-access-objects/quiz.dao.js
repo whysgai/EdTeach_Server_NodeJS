629c68ff7401aa4776aa7e2fa38d9697038ceed1
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 13:00:26 -0500

    A9 feat: Fetch questions when reading quiz by id

--- document-access-objects/quiz.dao.js
+++ document-access-objects/quiz.dao.js
@@ -2,7 +2,7 @@ const quizModel = require('../models/quizzes/quiz.model')
 
 const findAllQuizzes = () => quizModel.find()
 
-const findQuizById = (quizId) => quizModel.findById(quizId)
+const findQuizById = (quizId) => quizModel.findById(quizId).populate("questions").exec()
 
 const createQuiz = (newQuiz) => quizModel.create(newQuiz);
 
